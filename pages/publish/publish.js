e735abfaf6f49a23fd3d983cf3743653f8b183c1
chenxp38 <1160522425@qq.com>
2019-05-10 19:40:44 +0800

    小组项目，完成input内容获取，有一些还需要修改

--- pages/publish/publish.js
+++ pages/publish/publish.js
@@ -5,26 +5,43 @@ Page({
    * 页面的初始数据
    */
   data: {
+    /*命名时，为了方便记忆以及避免名字冲突，我将在快递，求助，跑腿，闲置的属性加上k,q,p,x的前缀*/
     id: '',
-    src_of_pic: '../../images /upload.png',/*这个是选择闲置物品图片后，将闲置物品图片替换原按钮图片*/
+    x_src_of_pic: '../../images /upload.png',/*这个是选择闲置物品图片后，将闲置物品图片替换原按钮图片*/
     /*以下是快递需要上传的属性*/
-    express_loc:'明德园test',
-    arrive_time:'2018/01/30 11:00:00',
-    loc:'至善园二号',
-    num:'4',
-    pay:'12',
-    remark:'none',
-    phone:'123456789',
-    wechat:'dsbsaj',
+    k_express_loc:'明德园test',
+    k_arrive_time:'2018/01/30 11:00:00',
+    k_loc:'至善园二号',
+    k_num:'4',
+    k_pay:'12',
+    k_remark:'none',
+    k_phone:'123456789',
+    k_wechat:'dsbsaj',
 
-     /*以下是闲置需要上传的属性*/
-    object_name:'',
-    content:'',
-    ending_time:'',
-    pay2:'',
-    phone2:'',
-    wechat:'',
-    wechat2:''
+    /*以下是求助需要上传的属性*/
+    q_title:'',
+    q_content:'',
+    q_ending_time:'',
+    q_pay:'',
+    q_phone:'',
+    q_wechat:'',
+    
+    /*以下是跑腿需要上传的属性*/
+    p_title:'',
+    p_content:'',
+    p_ending_time:'',
+    p_pay:'',
+    p_phone:'',
+    p_wechat:'',
+
+    /*以下是闲置需要上传的属性*/
+    x_object_name:'',
+    x_content:'',
+    x_ending_time:'',
+    x_pay:'',
+    x_phone:'',
+    x_wechat:'',
+    x_wechat:''
   },
 
   /**
@@ -35,7 +52,41 @@ Page({
     var date = new Date('2018/01/30 11:00:00');
     that.setData({
       id:options.id,
-      src_of_pic: '../../images/upload.png'
+      src_of_pic: '../../images/upload.png',
+      /*以下是快递需要上传的属性*/
+      k_express_loc: '明德园test',
+      k_arrive_time: '2018/01/30 11:00:00',
+      k_loc: '至善园二号',
+      k_num: '4',
+      k_pay: '12',
+      k_remark: 'none',
+      k_phone: '123456789',
+      k_wechat: 'dsbsaj',
+
+      /*以下是求助需要上传的属性*/
+      q_title: '',
+      q_content: '',
+      q_ending_time: '',
+      q_pay: '',
+      q_phone: '',
+      q_wechat: '',
+
+      /*以下是跑腿需要上传的属性*/
+      p_title: '',
+      p_content: '',
+      p_ending_time: '',
+      p_pay: '',
+      p_phone: '',
+      p_wechat: '',
+
+      /*以下是闲置需要上传的属性*/
+      x_object_name: '',
+      x_content: '',
+      x_ending_time: '',
+      x_pay: '',
+      x_phone: '',
+      x_wechat: '',
+      x_wechat: ''
     })
     console.log(that.data.id)
   },
@@ -116,20 +167,71 @@ Page({
       }
     })
   },
-  gotoupload: function () {
+  //获取快递输入的快递点
+  k_express_locInput: function (e) {
+    this.setData({
+      k_express_loc: e.detail.value
+    })
+  },
+  //获取快递输入的快递件数
+  k_numInput: function (e) {
+    this.setData({
+      k_num: e.detail.value
+    })
+  },
+  //获取快递输入的快递送达地址
+  k_locInput: function (e) {
+    this.setData({
+      k_loc: e.detail.value
+    })
+  },
+  //获取快递输入的快递送达时间
+  k_arrive_timeInput: function (e) {
+    this.setData({
+      k_arrive_time: e.detail.value
+    })
+  },
+  //获取快递输入的快递送达报酬
+  k_payInput: function (e) {
+    this.setData({
+      k_pay: e.detail.value
+    })
+  },
+  //获取快递输入的快递发布人微信号
+  k_wechatInput: function (e) {
+    this.setData({
+      k_wechat: e.detail.value
+    })
+  },
+  //获取快递输入的快递发布人手机号
+  k_phoneInput: function (e) {
+    this.setData({
+      k_phone: e.detail.value
+    })
+  },
+  //获取快递输入的快递发布人备注
+  k_remarkInput: function (e) {
+    this.setData({
+      k_remark: e.detail.value
+    })
+  },
 
+  
+  //快递的属性值上传
+  gotoupload: function () {
     wx.request({
       url: "http://172.18.32.138:8080/Create/Expressage",
       method: "POST",
       data: {
-        express_loc: '明德园test',
-        arrive_time: '2018/01/30 11:00:00',
-        loc: '至善园二号',
-        num: '4',
-        pay: '12',
-        remark: 'none',
-        phone: '123456789',
-        wechat: 'dsbsaj',
+        express_loc: k_express_loc,
+       // arrive_time: '2018/01/30 11:00:00',保留此行是为了保留date格式的时间
+        arrive_time:k_arrive_time,
+        loc: k_loc,
+        num: k_num,
+        pay: k_pay,
+        remark: k_remark,
+        phone: k_phone,
+        wechat: k_wechat,
       },
       header: {
         "Content-Type": "application/x-www-form-urlencoded"
