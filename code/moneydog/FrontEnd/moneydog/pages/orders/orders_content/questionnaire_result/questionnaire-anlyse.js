529f9247c5b8123d733bfd1a33807e771dcd2227
林锦竹 <957142389@qq.com>
2019-06-10 13:12:51 +0800

    问卷UI以及结果分析更新

--- code/moneydog/FrontEnd/moneydog/pages/orders/orders_content/questionnaire_result/questionnaire-anlyse.js
+++ code/moneydog/FrontEnd/moneydog/pages/orders/orders_content/questionnaire_result/questionnaire-anlyse.js
@@ -12,20 +12,25 @@ Page({
     showDialog: false, //弹窗
     questionItem: { type: -1, title: '', a: '', b: '', c: '', d: '' },
     questionList: [],
-    contentCount: []
+    contentCount: [],
+    jointResult: []
   },
-  onloadQuestionnaireResult: function() {
-    var that = this
-    wx.request({
-      url: "https://moneydog.club:3030/Create/viewAll",
-      header: { sessionId: that.data.sessionId },
-      data: { id: that.data.orderid },
-      success: function (res) {
-        console.log(res.data.viewAllList.content_count)
-      }
+  jointQuestionnaireResult: function() {
+    for(var i = 0;i < this.data.questionList.length;i++) {
+      var resultItem = {question: this.data.questionList[i], result: this.data.contentCount[i]}
+      this.data.jointResult.push(resultItem)
+    }
+    this.setData({
+      jointResult: this.data.jointResult
     })
   },
-
+  showCompletionResult: function(e) {
+    var fill = JSON.stringify(this.data.contentCount[e.currentTarget.dataset.id].fill)
+    console.log(fill)
+    wx.navigateTo({
+      url: 'completion_results?fill=' + fill,
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -33,10 +38,12 @@ Page({
     this.setData({
       questionnaireName: wx.getStorageSync('questionnaireName'),
       questionnaireDesc: wx.getStorageSync('questionnaireDesc'),
-      questionnairePay: wx.getStorageSync('questionnairePay'),
       questionnaireTota: wx.getStorageSync('questionnaireTota'),
-      questionList: wx.getStorageSync('questionList')
+      questionList: wx.getStorageSync('questionList'),
+      contentCount: wx.getStorageSync('questionContentCountList'),
     })
+    this.jointQuestionnaireResult()
+    console.log(this.data.jointResult)
   },
 
   /**
